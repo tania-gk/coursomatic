82a491bca81ddde94c55682565e79c10b331a34f
tania gabbay <tania.gabbay@gmail.com>
2017-07-26 21:15:26 +0300

    User model defined and checked

--- app/app.js
+++ app/app.js
@@ -1,5 +1,24 @@
 var coursoramaApp = angular.module("coursoramaApp", ["ngRoute", "ngAnimate", "ui.bootstrap"]);
 
-coursoramaApp.controller("MainCtrl", function ($scope){
+coursoramaApp.controller("MainCtrl", function ($scope,User,activeUser){
+    var fixedUser = {
+    "id":"0",
+    "firstName":"Sigal",
+    "lastName":"Yaron",
+    "phone":"0545723644",
+    "city":"Holon",
+    "email":"sigalyaron10@gmail.com",
+    "password":"11111",
+    "type":"teacher",
+    "course":""
+    };
+    var myUser = fixedUser;
+    console.log(JSON.stringify(myUser));
+     console.log(activeUser.isLoggedIn());
+     activeUser.login(myUser);
+     console.log(JSON.stringify(activeUser.get()));
+     console.log(activeUser.isLoggedIn());
+     activeUser.logout();
+     console.log(activeUser.isLoggedIn());
 
 });
---
+++ model/users.js
@@ -0,0 +1,43 @@
+//service constructor
+coursoramaApp.factory("User", function () {
+    function User(plainObject) {
+        this.id = plainObject.id;
+        this.firstName = plainObject.firstName;
+        this.lastName = plainObject.lastName;
+        this.phone = plainObject.phone;
+        this.city = plainObject.city;
+        this.email = plainObject.email;
+        this.password = plainObject.password;
+        this.type = plainObject.type;
+        this.course = plainObject.course;
+    };
+    return User;
+});
+
+// Service that manges users
+coursoramaApp.factory("activeUser", function () {
+    var user = null;
+
+    var isLoggedIn = function () {
+        return user ? true : false;
+    };
+
+    var login = function (loggedInUser) {
+        user = loggedInUser;
+    };
+
+    var logout = function () {
+        user = null;
+    };
+
+    var get = function () {
+        return user;
+    };
+
+    return {
+        isLoggedIn: isLoggedIn,
+        login: login,
+        logout: logout,
+        get: get
+    };
+});
